57da850246157c145633f769fd95103e7a0774df
Baggins <redblue@inbox.ru>
2016-04-19 01:40:09 +0300

    refactoring

--- src/com/fedorbaggins/myseabattle/Main/Main.java
+++ src/com/fedorbaggins/myseabattle/Main/Main.java
@@ -2,7 +2,8 @@ package com.fedorbaggins.myseabattle.Main;
 import com.fedorbaggins.myseabattle.game.Game;
 
 public class Main {
+    private static Game game;
     public static void main(String[] args) {
-        Game game = new Game();
+        game = new Game();
     }
 }
