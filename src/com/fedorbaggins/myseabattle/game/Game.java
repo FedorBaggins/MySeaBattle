91dca9ef3b595943914c4fac137ca5c8d3f4c18d
Baggins <redblue@inbox.ru>
2016-04-19 01:19:19 +0300

    Created messages of begining and end of the game

--- src/com/fedorbaggins/myseabattle/game/Game.java
+++ src/com/fedorbaggins/myseabattle/game/Game.java
@@ -2,4 +2,8 @@ package com.fedorbaggins.myseabattle.game;
 
 
 public class Game {
+    public Game(){
+        System.out.println("Game started");
+        System.out.println("Game over");
+    }
 }
