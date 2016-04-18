82b620c0b3278da22c15c3462d1851888b8865f8
Baggins <redblue@inbox.ru>
2016-04-19 01:20:11 +0300

    Game created

--- src/com/fedorbaggins/myseabattle/Main/Main.java
+++ src/com/fedorbaggins/myseabattle/Main/Main.java
@@ -1,8 +1,8 @@
 package com.fedorbaggins.myseabattle.Main;
-
+import com.fedorbaggins.myseabattle.game.Game;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Game game = new Game();
     }
 }
