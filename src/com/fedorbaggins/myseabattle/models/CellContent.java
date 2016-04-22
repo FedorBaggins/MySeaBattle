59eda67a7167cca4d0e30dc2bd488b13f055f39d
Baggins <redblue@inbox.ru>
2016-04-23 00:17:44 +0300

    remove

--- src/com/fedorbaggins/myseabattle/models/CellContent.java
+++
@@ -1,5 +0,0 @@
-package com.fedorbaggins.myseabattle.models;
-
-
-public enum CellContent {
-}
---
+++ tst/com/fedorbaggins/myseabattle/models/FieldTest.java
@@ -0,0 +1,10 @@
+package com.fedorbaggins.myseabattle.models;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sumkin on 21.04.2016.
+ */
+public class FieldTest {
+
+}
