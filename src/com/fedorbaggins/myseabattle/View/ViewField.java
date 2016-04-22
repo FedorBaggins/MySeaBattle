cbb381c7dceebcb090dcb653f91efd230c08dce1
Baggins <redblue@inbox.ru>
2016-04-23 00:22:20 +0300

    ViewField method

--- src/com/fedorbaggins/myseabattle/View/ViewField.java
+++ src/com/fedorbaggins/myseabattle/View/ViewField.java
@@ -1,5 +1,17 @@
 package com.fedorbaggins.myseabattle.View;
 
 
+import com.fedorbaggins.myseabattle.models.Cell;
+import com.fedorbaggins.myseabattle.models.Field;
+
 public class ViewField {
+    public static void viewField(Cell[][] field){
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j != 9) System.out.printf("%3s", field[j][i]);
+                else System.out.printf("%3s%n", field[j][i]);
+            }
+        }
+    }
+
 }
